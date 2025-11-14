a20afba653c6622288c2725f87ec3ac9a6ffceaf
Esox-rutilus <jesha56@live.dk>
2025-11-14 11:16:25 +0100

    Added FILEINDEX as a replacement, which will count the number of files in the target folder, and use that number + 1

--- src/main.ts
+++ src/main.ts
@@ -86,7 +86,6 @@ const absOutputPath = useNoWrapper
   : path.resolve(cwd, outputPath, name);
 
 fs.mkdirSync(absOutputPath, { recursive: true });
-
 const replacements = {
   NAME: name,
   NAMEPASCAL: name
@@ -94,6 +93,7 @@ const replacements = {
     .map((x) => x.charAt(0).toUpperCase() + x.slice(1))
     .join(""),
   DATE: new Date().toLocaleDateString(),
+  FILEINDEX: `${fs.readdirSync(absOutputPath).length + 1}`
 };
 
 function replacePlaceholders(str: string, replacements: Record<string, string>): string {
